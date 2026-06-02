048b56074363619df77080ebe699bf60b1741642
Mohsen Zare <you@example.com>
2026-06-03 02:08:38 +0330

    New Roadmap - Complete Redo!!

--- shared/supabase/client.ts
+++ shared/supabase/client.ts
@@ -5,6 +5,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+console.log("URL =", supabaseUrl);
+console.log("KEY EXISTS =", !!supabaseAnonKey);
+
 // جلوگیری از crash در build (Vercel SSR phase)
 if (!supabaseUrl || !supabaseAnonKey) {
   console.warn("Supabase env vars are missing");
