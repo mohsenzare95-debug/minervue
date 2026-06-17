8e483e543d850dda14aec3126530ca7fe44e536b
Mohsen Zare <you@example.com>
2026-06-17 23:18:51 +0330

    posthog instal

--- features/analytics/posthog.ts
+++ features/analytics/posthog.ts
@@ -1,13 +1,15 @@
-'use client';
+'use client'
 
-import posthog from 'posthog-js';
+import posthog from 'posthog-js'
 
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
+export function initPostHog() {
+  if (typeof window === 'undefined') return
+
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     capture_pageview: true,
     capture_pageleave: true,
-  });
+  })
 }
 
-export default posthog;
+export default posthog
