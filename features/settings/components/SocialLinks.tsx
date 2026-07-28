6d24bd7ea4fd3ffc7cb13283da86f0d72a2940d4
Mohsen Zare <you@example.com>
2026-07-28 05:36:02 +0330

    New Cornea Cards

--- features/settings/components/SocialLinks.tsx
+++ features/settings/components/SocialLinks.tsx
@@ -25,7 +25,7 @@ export function SocialLinks() {
 
       {/* TELEGRAM */}
       <a
-        href="https://t.me/visosage"
+        href="https://t.me/iceburgins"
         target="_blank"
         rel="noreferrer"
         style={styles.iconBtn}
